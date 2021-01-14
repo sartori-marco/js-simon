be536ba1f2d824dd4915ec56981a75eac52a31db
marco sartori <marcosartori98@yahoo.com>
2021-01-14 15:22:33 +0100

    add alert per 5 numeri random

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-$(document).ready(function(){
+// Un alert espone 5 numeri casuali.
+// Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire un prompt alla volta i numeri che ha visto precedentemente.
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
 
 
 
-  
-});
+// genero i 5 numeri random in un alert
+var numeri = [numeriRandom(1, 100),numeriRandom(1, 100), numeriRandom(1, 100), numeriRandom(1, 100), numeriRandom(1, 100)];
+
+alert(numeri);
+
+console.log(numeri);
+
+
+// funzione numeri random
+function numeriRandom(min, max){
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
